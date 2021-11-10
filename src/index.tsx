486b46b1d22347480b6e9373a8f743620a127698
LinsThi <thiagolins13255@gmail.com>
2021-11-10 14:43:50 -0300

    feat: creating LoginScreen

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+
+import { Login } from './screens/Login';
 
 const App: React.FC = () => {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <>
       <StatusBar style="auto" />
-      <Text>Arquivo TS!</Text>
-    </View>
+      <Login />
+    </>
   );
 };
 
---
+++ src/screens/Login/styles.ts
@@ -0,0 +1,27 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background: #4299e1;
+`;
+
+export const ContainerImage = styled.View`
+  flex: 0.3;
+  justify-content: center;
+`;
+
+export const ContainerInputs = styled.View`
+  flex: 0.4;
+  justify-content: center;
+`;
+
+export const ContainerButton = styled.View`
+  flex: 0.1;
+`;
+
+export const LogoImage = styled.Image`
+  width: 200px;
+  height: 200px;
+`;
