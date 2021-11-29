2b446e9d0fce9f2fb2b8cfd95000df3f01144718
LinsThi <thiagolins13255@gmail.com>
2021-11-29 16:30:29 -0300

    Create Logic Baseboard

---
+++ src/components/BaseBoard/utils/index.ts
@@ -0,0 +1,30 @@
+import type { GroceryProps } from '~/@types/Entity/Grocery';
+
+export const countAllItens = (groceryList: GroceryProps[]) => {
+  let count = 0;
+  const listCount = groceryList;
+
+  listCount.map(categoryCurrent => {
+    count += categoryCurrent.listItems.length;
+    return null;
+  });
+
+  return count;
+};
+
+export const amountList = (groceryList: GroceryProps[]) => {
+  let amount = 0;
+  const listCount = groceryList;
+
+  listCount.map(categoryCurrent => {
+    categoryCurrent.listItems.map(itemsCurrent => {
+      const value = itemsCurrent.price;
+      amount += parseInt(value, 10);
+      return null;
+    });
+
+    return null;
+  });
+
+  return amount;
+};
